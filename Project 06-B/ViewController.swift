e5d6033f9f2c300521471eb55f29ac02ab09ebf8
Suraj Meghwar <isurajparmar85@gmail.com>
2024-06-03 06:58:19 -0700

    Initial Commit

---
+++ Project 06-B/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 06-B
+//
+//  Created by isurajparmar85 on 03/06/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Project 06-BTests/Project_06_BTests.swift
@@ -0,0 +1,33 @@
+//
+//  Project_06_BTests.swift
+//  Project 06-BTests
+//
+//  Created by isurajparmar85 on 03/06/2024.
+//
+
+import XCTest
+@testable import Project_06_B
+
+class Project_06_BTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
